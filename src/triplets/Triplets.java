381389955de38e41d04dd6a5197bb2ca13275a95
blhad3491 <blhad3491@7KXL6Y1.Student.UGDSB.ED>
2018-09-17 15:15:23 -0400

    Rhyming words

---
+++ src/triplets/Triplets.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package triplets;
+import java.util.ArrayList;
+/**
+ *
+ * @author blhad3491
+ */
+public class Triplets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
