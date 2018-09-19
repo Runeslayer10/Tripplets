2f95f972219c89d95f72534979db92f4c2930d76
Runeslayer10 <43037188+Runeslayer10@users.noreply.github.com>
2018-09-18 20:38:27 -0400

    Update Triplets.java

--- src/triplets/Triplets.java
+++ src/triplets/Triplets.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Blake Hadaway
+ * September 18th, 2018
+ * Writing a random 3 line poem.
  */
 
 package triplets;
@@ -17,68 +17,79 @@ public class Triplets {
      */
     public static void main(String[] args) {
         ArrayList<String> nouns = new ArrayList<String>();
-    nouns.add("cat");
-    nouns.add("dog");
-    nouns.add("horse");
-    nouns.add("goat");
-    nouns.add("eagle");
-    nouns.add("lion");
-    nouns.add("giraffe");
+        nouns.add("cat");
+        nouns.add("dog");
+        nouns.add("horse");
+        nouns.add("goat");
+        nouns.add("eagle");
+        nouns.add("lion");
+        nouns.add("giraffe");
 
-    ArrayList<String> verbs = new ArrayList<String>();
-    verbs.add("ate");
-    verbs.add("slept");
-    verbs.add("jumped");
-    verbs.add("raced");
-    verbs.add("ran");
-    verbs.add("tripped");
-    verbs.add("guessed");
-    verbs.add("tapped");
+        ArrayList<String> verbs = new ArrayList<String>();
+        verbs.add("ate");
+        verbs.add("slept");
+        verbs.add("jumped");
+        verbs.add("raced");
+        verbs.add("ran");
+        verbs.add("tripped");
+        verbs.add("guessed");
+        verbs.add("tapped");
 
-    ArrayList<String> rhymingNouns = new ArrayList<String>();
-    rhymingNouns.add("mouse");
-    rhymingNouns.add("blouse");
-    rhymingNouns.add("grouse");
-    rhymingNouns.add("house");
-    rhymingNouns.add("spouse");
-    rhymingNouns.add("doghouse");
-    rhymingNouns.add("beachhouse");
-    rhymingNouns.add("treehouse");
+        ArrayList<String> rhymingNouns = new ArrayList<String>();
+        rhymingNouns.add("mouse");
+        rhymingNouns.add("blouse");
+        rhymingNouns.add("grouse");
+        rhymingNouns.add("house");
+        rhymingNouns.add("spouse");
+        rhymingNouns.add("doghouse");
+        rhymingNouns.add("beachhouse");
+        rhymingNouns.add("treehouse");
 
-    String noun;
-    String verb1, verb2;
-    String rhymingNoun1, rhymingNoun2;
-    int randomNum;
+        String noun;
+        String verb1, verb2, verb3;
+        String rhymingNoun1, rhymingNoun2, rhymingNoun3;
+        int randomNum;
 
-    //get a random noun from the nouns array
-    //use .size() to get the # of elements
-    randomNum = (int) (Math.random() * nouns.size());
-    noun = nouns.get(randomNum);
+        //get a random noun from the nouns array
+        //use .size() to get the # of elements
+        randomNum = (int) (Math.random() * nouns.size());
+        noun = nouns.get(randomNum);
 
-    //get the first random verb from the verbs array &
-    // remove from ArrayList
-    randomNum = (int) (Math.random() * verbs.size());
-    verb1 = verbs.get(randomNum);
-    verbs.remove(randomNum);
+        //get the first random verb from the verbs array &
+        // remove from ArrayList
+        randomNum = (int) (Math.random() * verbs.size());
+        verb1 = verbs.get(randomNum);
+        verbs.remove(randomNum);
 
-    //get the second random verb from verbs array and remove it
-    randomNum = (int) (Math.random() * verbs.size());
-    verb2 = verbs.get(randomNum);
-    verbs.remove(randomNum);
+        //get the second random verb from verbs array and remove it
+        randomNum = (int) (Math.random() * verbs.size());
+        verb2 = verbs.get(randomNum);
+        verbs.remove(randomNum);
+        
+        //get the second random verb from verbs array and remove it
+        randomNum = (int) (Math.random() * verbs.size());
+        verb3 = verbs.get(randomNum);
+        verbs.remove(randomNum);
 
-    //get the first rhyming noun and remove it
-    randomNum = (int) (Math.random() * rhymingNouns.size());
-    rhymingNoun1 = rhymingNouns.get(randomNum);
-    rhymingNouns.remove(randomNum);
+        //get the first rhyming noun and remove it
+        randomNum = (int) (Math.random() * rhymingNouns.size());
+        rhymingNoun1 = rhymingNouns.get(randomNum);
+        rhymingNouns.remove(randomNum);
 
-    //get the second rhyming noun and remove it
-    randomNum = (int) (Math.random() * rhymingNouns.size());
-    rhymingNoun2 = rhymingNouns.get(randomNum);
-    rhymingNouns.remove(randomNum);
+        //get the second rhyming noun and remove it
+        randomNum = (int) (Math.random() * rhymingNouns.size());
+        rhymingNoun2 = rhymingNouns.get(randomNum);
+        rhymingNouns.remove(randomNum);
+        
+        //get the second rhyming noun and remove it
+        randomNum = (int) (Math.random() * rhymingNouns.size());
+        rhymingNoun3 = rhymingNouns.get(randomNum);
+        rhymingNouns.remove(randomNum);
 
-    //Display the poem
-    System.out.println("The " + noun + " " + verb1 + " a " + 
-      rhymingNoun1 + "\n" + "And then " + verb2 + 
-      " it in the " + rhymingNoun2);
+        //Display the poem
+        System.out.println("The " + noun + " " + verb1 + " a " + 
+          rhymingNoun1 + "\n" + "And then " + verb2 + 
+          " it in the " + rhymingNoun2 + "\n" + "finally the " 
+        + noun + " " + verb3 + " the " + rhymingNoun3);
     }
 }
